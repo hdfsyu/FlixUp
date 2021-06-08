e6b6a5247483bbc6aabf3b6fa80699709c0dfed0
Anton Ego <viyaanchauhan@hotmail.com>
2021-06-08 11:18:42 -0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,22 +1,47 @@
-package;
-
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.input.keyboard.FlxKey;
+import flixel.input.mouse.FlxMouseEventManager;
 
 class PlayState extends FlxState {
 	var sprite:FlxSprite;
-	var scaleFactor = 0.1;
 
 	override public function create():Void {
 		super.create();
-		sprite = new FlxSprite(AssetPaths.player__png);
-		sprite.x = FlxG.width / 2 - sprite.width / 2;
-		sprite.y = FlxG.height / 2 - sprite.height / 2;
+		sprite = new FlxSprite();
+		sprite.loadGraphic(AssetPaths.player__png);
+		sprite.x = 200;
+		sprite.y = 200;
 		add(sprite);
+		FlxMouseEventManager.add(sprite, function(s:FlxSprite) {
+			trace("MouseDown");
+		}, function(s:FlxSprite) {
+			trace("MouseUp");
+		}, function(s:FlxSprite) {
+			trace("MouseOver");
+		}, onMouseOut);
 	}
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
+	}
+
+	public function onMouseOut(s:FlxSprite) {
+		trace("Mouse Out, sprite at: " + s.x + "," + s.y + " Mouse at: " + FlxG.mouse.getPosition());
+		if (FlxG.keys.enabled) {
+			if (FlxG.keys.pressed.LEFT)
+				sprite.x--;
+			if (FlxG.keys.pressed.RIGHT)
+				sprite.x++;
+			if (FlxG.keys.justReleased.UP)
+				sprite.y--;
+			if (FlxG.keys.justReleased.DOWN)
+				sprite.y++;
+			if (FlxG.keys.anyPressed([FlxKey.ESCAPE, FlxKey.SPACE])) {
+				sprite.x = FlxG.width / 2 - sprite.width / 2;
+				sprite.y = FlxG.height / 2 - sprite.height / 2;
+			}
+		}
 	}
 }
